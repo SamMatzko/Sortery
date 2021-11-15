d43eda09d96cc65ed363b8328e0cda650dc77ab7
Samuel Matzko <matzko+sam@gmail.com>
2021-11-14 19:10:48 -0500

    Started the help message and added an unknown-argument handler.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,51 @@
 use std::env;
-use std::io;
+
+fn help(version_number: String) {
+    // Display the help messages and exit
+
+    println!("Coral {}\n", version_number);
+    println!("Coral is a free, open-source file and directory sorter written");
+    println!("in Rust.\n");
+    println!("Command-line options:\n");
+    print!("-h, --help\t");
+    println!("Show this message and quit.");
+}
+
+fn unknown_option(arg: &String) {
+    // Print the "unknown option" message and exit
+
+    println!("Error: unknown option: {}", arg);
+}
 
 fn main() {
-    println!("Coral 0.0.0");
+
+    // Program constants
+    let options = [
+        String::from("-h"),
+        String::from("--help")
+    ];
+    let version_number = "0.0.0";
 
     // The command-line arguments
     let args: Vec<String> = env::args().collect();
+    let args = &args[1..];
+
+    println!("{:?}", args);
+
+    // If there are no arguments, show the help message
+    if args.len() == 0 {
+        help((&version_number).to_string());
+        return;
+    }
+    
+    // Check all arguments for validity
+    for option in args {
+        if !options.contains(option) {
+            unknown_option(option);
+        }
+    }
+    
+    if args.contains(&options[0]) || args.contains(&options[1]) {
+        help((&version_number).to_string());
+    }
 }
