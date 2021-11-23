b313dfa2cce1cba0b8a5fe1aeb74fffda43a86cf
Samuel Matzko <matzko+sam@gmail.com>
2021-11-23 17:08:05 -0500

    0.1.2 bump.

--- src/messages.rs
+++ src/messages.rs
@@ -76,7 +76,7 @@ pub mod info_messages {
     pub fn help() {
         // Display the help messages and exit
 
-        let version_number = String::from("0.0.2");
+        let version_number = String::from("0.1.2");
     
         println!("Coral {}\n", version_number);
         println!("Coral is a free, open-source file and directory sorter written");
