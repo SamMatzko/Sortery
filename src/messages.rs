dd2d669b7d01b1fa12505b7efb30742cdb18e313
Samuel Matzko <matzko+sam@gmail.com>
2021-12-04 17:18:57 -0500

    Made the progress bar part of messages.rs, so that multiple tools can use it for status display.

--- src/messages.rs
+++ src/messages.rs
@@ -1,3 +1,44 @@
+use std::{io, io::Write};
+
+// The progress bar used when sorting
+pub struct ProgressBar {
+    pub completed_message: String,
+    pub message: String,
+    pub total: usize,
+}
+impl ProgressBar {
+
+    pub fn set_progress(&self, completed: usize) {
+        // Print the progress bar
+
+        let progress_done: usize = ((20f32/self.total as f32)*completed as f32) as usize;
+        let progress_todo: usize = (20f32-((20f32/(self.total-1) as f32)*completed as f32)) as usize;
+
+        print!(
+            "{0} |{1}{2}| {3}% {4}/{5}{6}\r",
+            self.message,
+            "⌷".repeat(progress_done),
+            "-".repeat(progress_todo),
+            ((100f32/self.total as f32)*completed as f32) as usize,
+            completed,
+            self.total,
+            " ".repeat(15)
+        );
+        io::stdout().flush().expect("Failed to flush stdout.");
+    }
+
+    pub fn complete(&self) {
+        // Show the progress bar at completed status
+        println!(
+            "{0} |{1}| 100% {2}/{2}{3}",
+            self.completed_message,
+            "⌷".repeat(20),
+            self.total,
+            " ".repeat(15)
+        )
+    }
+}
+
 pub mod error_messages {
     // Error message templates for common errors
     
